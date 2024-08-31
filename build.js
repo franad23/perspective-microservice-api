7515cae6bed352e5f16535e5545e625d482972b8
Franco Diaz <dfrancoandres@gmail.com>
2024-08-30 22:44:54 -0300

    update github action

--- build.js
+++ build.js
@@ -1,39 +1,16 @@
-const fs = require("fs");
-const path = require("path");
+// esbuild.config.js
+
 const esbuild = require("esbuild");
+const path = require("path");
 
-// Ruta al archivo .env
-const envFilePath = path.resolve(__dirname, ".env");
-
-// Leer el archivo .env
-const envConfig = fs.existsSync(envFilePath)
-  ? fs.readFileSync(envFilePath, "utf8")
-  : "";
-const envVars = envConfig
-  .split("\n")
-  .filter((line) => line.trim() !== "" && !line.startsWith("#"))
-  .reduce((acc, line) => {
-    const [key, value] = line.split("=");
-    acc[key] = value;
-    return acc;
-  }, {});
-
-// Configurar variables para esbuild
-const define = Object.keys(envVars).reduce((defs, key) => {
-  defs[`process.env.${key}`] = JSON.stringify(envVars[key]);
-  return defs;
-}, {});
-
-// Ejecutar esbuild con las variables de entorno definidas
 esbuild
   .build({
-    entryPoints: ["src/index.js"],
+    entryPoints: ["src/index.js"], // Cambia a la ruta de tu archivo de entrada principal
     bundle: true,
-    platform: "node",
-    target: "node14",
-    outfile: "dist/index.js",
-    sourcemap: true,
-    minify: true,
-    define,
+    platform: "node", // Especifica que estás construyendo para Node.js
+    target: "node20", // Ajusta esto a la versión de Node.js que estás utilizando
+    outdir: "dist", // Directorio de salida para los archivos construidos
+    sourcemap: false, // Opcional: Genera archivos de mapa de fuente para depuración
+    minify: false, // Opcional: Minifica el código, establece en true si deseas minificar
   })
   .catch(() => process.exit(1));
